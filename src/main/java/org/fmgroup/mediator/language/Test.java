2354b408ec06155b8c9efa74f05f84ca831f1b31
xue-xy <35564615+xue-xy@users.noreply.github.com>
2020-12-21 20:39:58 +0800

    Add hybrid system example, two water tanks.

--- src/main/java/org/fmgroup/mediator/language/Test.java
+++ src/main/java/org/fmgroup/mediator/language/Test.java
@@ -1,17 +1,22 @@
 package org.fmgroup.mediator.language;
 
 import org.fmgroup.mediator.language.Program;
+import org.fmgroup.mediator.plugins.scheduler.Scheduler;
+import org.fmgroup.mediator.language.entity.automaton.Automaton;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws FileNotFoundException {
-//        String filename = "resources\\models\\testbench.med";
-        String filename = "resources\\models\\test\\hybridsingle.med";
+    public static void main(String[] args) throws FileNotFoundException, ValidationException {
+//        String filename = "resources/models/testbench.med";
+//        String filename = "resources\\models\\test\\hybridsingle.med";
+        String filename = "resources/models/test/watertank2.med";
 //        List<String> expath = new ArrayList<String>();
         Program prog = Program.parseFile(filename);
-        System.out.println(prog.toString());
+
+        Automaton sysAutomaton = Scheduler.Schedule(prog.getSystems().get("twotanks"));
+        System.out.println(sysAutomaton);
     }
 }
